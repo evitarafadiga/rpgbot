89663db33a3654f3cfe6bdbdb10a1fa636039822
Evitarafadiga <lazvsantos@gmail.com>
2019-11-14 23:48:54 -0300

    Primeiras alteraçoes no chatbot emolga

---
+++ emolga.rb
@@ -0,0 +1,21 @@
+require 'telegram_bot'
+
+token = 'inserir token'
+
+bot = TelegramBot.new(token: token)
+
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+
+    message.reply do |reply|
+        case command
+        when /start/i
+      reply.text = "Emo mo!"
+      puts "sending #{reply.text.inspect} to @#{message.from.username}"
+      reply.send_with(bot)
+        else
+        reply.text = ["Eu não entendi o que raios #{command.inspect} significa."]
+        end 
+    end
+end 
