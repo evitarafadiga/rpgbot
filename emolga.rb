054ec7876b4cf8395973d285bf16bb1ed888d813
Evitarafadiga <lazvsantos@gmail.com>
2019-11-23 00:07:23 -0300

    Custom keyboards adicionado.

--- emolga.rb
+++ emolga.rb
@@ -8,6 +8,22 @@ bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
 
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      question = 'London is a capital of which country?'
+      # See more: https://core.telegram.org/bots/api#replykeyboardmarkup
+      answers =
+        Telegram::Bot::Types::ReplyKeyboardMarkup
+        .new(keyboard: [%w(A B), %w(C D)], one_time_keyboard: true)
+      bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+    when '/stop'
+      # See more: https://core.telegram.org/bots/api#replykeyboardremove
+      kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
+      bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
+    end
+  end
+
     message.reply do |reply|
         case command
         when /start/i
@@ -22,7 +38,7 @@ bot.get_updates(fail_silently: true) do |message|
         when /meudesafio/i
         opdesafio = ['Nada sobre nada por emoenquanto...','Depois retornamos nesse emo assunto.','Hmf, err. Nada. Mo!']
         reply.text = "#{opdesafio.sample.capitalize}, #{message.from.first_name}!"
-        
+
         when /presente/i
         oppresente = ['Nada de especial por enquanto...']
         reply.text = "#{oppresente.sample.capitalize}, #{message.from.first_name}!"
@@ -36,11 +52,10 @@ bot.get_updates(fail_silently: true) do |message|
         opinicial = ['Grookey','Scorbunny','Sobble','Snom','Rookidee','Blipbug','Rolycoly','Mime Jr','Hatenna','Impidimp']
         reply.text = "#{opinicial.sample.capitalize}, #{message.from.first_name}!"
 
-
         else
         reply.text = ["Emo, o que, mo #{command.inspect} significa??!."]
         end
         puts "sending #{reply.text.inspect} to @#{message.from.username}"
-        reply.send_with(bot) 
+        reply.send_with(bot)
     end
-end 
+end
