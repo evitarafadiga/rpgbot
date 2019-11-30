15327e9461e941ee71d2cf0f44862162f458bcca
Evitarafadiga <lazvsantos@gmail.com>
2019-11-29 23:48:44 -0300

    Alterações no bot Emolga 001

--- emolga/emolga.rb
+++ emolga/emolga.rb
@@ -37,7 +37,8 @@ bot.get_updates(fail_silently: true) do |message|
         /minhadica02
         /minhadica03
         /minhadica04
-        /minhadica05']
+        /minhadica05
+        /minhadica06']
         reply.text = "#{opcacaaotesouro.sample.capitalize}, #{message.from.first_name}!"
 
         when /meuinicial/i
