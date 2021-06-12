b88e7c280d84f6c4a8ffbc159c1004b549d4beaf
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2021-06-12 02:47:07 -0300

    Adição do JDA
    em pasta do projeto.

--- emolga-rpg-bot/src/rpg/Emolga.java
+++ emolga-rpg-bot/src/rpg/Emolga.java
@@ -2,7 +2,6 @@ package rpg;
 
 import javax.security.auth.login.LoginException;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
@@ -11,7 +10,7 @@ public class Emolga {
 	static JDA builder = null;
 	
 	public static void main(String [] args) throws LoginException{
-		JDA builder = JDABuilder.createDefault("ODQ0MjMzNzA5MzA4NjA4NTQy.YKPb6Q.KbhJNNpvOzbJxd2ecUaipIcGUFw").build();
+		JDA builder = JDABuilder.createDefault("token").build();
 		
 	}
 
